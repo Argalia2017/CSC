5dc47f4f6759e514cc9cd364a8a7da075cfa1575
csc <870177103@qq.com>
2023-05-01 16:50:41 +0800

    202305

--- src/test.cpp
+++ src/test.cpp
@@ -1,15 +1,1 @@
-﻿#include "util.h"
-
-#include "csc/csc_end.h"
-#include <random>
-#include "csc/csc_begin.h"
-
-using namespace CSC ;
-
-/*
-1. 减少相同内存布局的成员函数数量
-*/
-
-exports void test_main () {
-
-}
+﻿
